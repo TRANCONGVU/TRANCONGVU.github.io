a7a6daec146a1b3702e6efa727e1043b546dc1ee
TRANCONGVU <vndvip10>
2019-02-20 00:36:25 +0700

    fix

--- js_1/do_an.js
+++ js_1/do_an.js
@@ -1,9 +1,9 @@
  function myFunction() {
-                           document.getElementById("color").innerHTML ='<img style="width: 440px;margin-left: 30px;" src="images/xam1.jpg">';
+                           document.getElementById("color").innerHTML ='<img style="width: 440px;margin-left: 30px;" src="images/bo11.jpg">';
                           
                      }
 
 function Function() {
-                         	document.getElementById("color").innerHTML = '<img style="width: 440px;margin-left: 30px;" src="images/sp1.jpg">';
+                         	document.getElementById("color").innerHTML = '<img style="width: 440px;margin-left: 30px;" src="images/bo1_hong.jpg">';
                            
                           		}
